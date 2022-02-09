85120ba4ba84d11ae8d3eff07c1e4d4ab4e15026
Jeff Grover <jeffrey.grover13@gmail.com>
2022-02-09 13:05:17 -0600

    tweaking to get around payload limit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const config = {
 
 var app = express();
   app.use(express.json({limit: '50mb'}));
-  app.use(express.urlencoded({limit: '50mb', extended:true}));
+  app.use(express.urlencoded({limit: '50mb', extended:true, parameterLimit:50000}));
 
 
 const T = new Twitter(config);
